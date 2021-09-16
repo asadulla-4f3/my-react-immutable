0daad9182040b5f5ecbedebcb68b162c27b4826d
asadulla-4f3 <52310096+asadulla-4f3@users.noreply.github.com>
2021-09-16 23:48:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import './App.css';
+import { List } from 'immutable';
+class App extends Component {
+  // constructor(){
+  //   super();
+  // }
+
+  render() {
+    // const list1 = List([1, 2]);
+    // const list2 = list1.push(3, 4, 5);
+    // const list3 = list2.unshift(0);
+    // const list4 = list1.concat(list2, list3);
+    // console.log(list1.size, 'list1');
+    // console.log(list2.size, 'list2');
+    // console.log(list3.size, 'list3');
+    // console.log(list4.size, 'list4');
+
+    return (
+      <div>Hello world!
+        Hi brother! Hello hey Hi
+      </div>
+    );
+  }
+}
+
+export default App;
